f917d8462cd624bff4415dce90284d1717078244
zhailei <zhl@stu.scu.edu.cn>
2024-03-10 23:35:18 +0800

    normal operation

--- widgets/source/DAO.cpp
+++ widgets/source/DAO.cpp
@@ -4,24 +4,35 @@
 #include <QSqlError>
 #include <QMessageBox>
 
-static QString dataBaseStr(QString("DRIVER={Microsoft Access Driver (*.mdb)};FIL={MS Access};DBQ=%1")
-                               .arg("D:\\Development\\TRTime\\dataBase\\TRTimeRecord.mdb"));
+static QString dataBaseStr(QString("DRIVER={Microsoft Access Driver (*.mdb, *accdb)};FIL={MS Access};DBQ=%1;Uid=%2,Pwd=%3")
+                               .arg("D:\\Development\\TRTime\\dataBase\\TRTimeRecord.accdb")
+                               .arg("hfcim")
+                               .arg("hfcim"));
 
 DAO* DAO::thePtr = nullptr;
 DAO::GbClear DAO::m_GbClear;
 
 DAO::DAO(){
     this->clear();
-    this->theDataBase = new QSqlDatabase(QSqlDatabase::addDatabase("QODBC"));
-
+    /*
+    this->theDataBase = new QSqlDatabase();
+    this->theDataBase->addDatabase("QODBC");
     this->theDataBase->setDatabaseName(dataBaseStr);
+*/
 
+    QSqlDatabase sq = QSqlDatabase::addDatabase("QODBC");
+    sq.setDatabaseName(dataBaseStr);
+
+    /*
     if(!this->theDataBase->isOpen()){
         QMessageBox::information(nullptr, "Error",this->theDataBase->lastError().text());
-
-    }else{
-        QMessageBox::information(nullptr, "Success", "DataBase connect success");
     }
+    */
+
+    if(!sq.isOpen()){
+        QMessageBox::information(nullptr, "Error",sq.lastError().text());
+    }
+
 }
 
 DAO::~DAO(){
